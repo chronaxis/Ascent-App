0f5f1b721b5ed66c24d25f5d8e277fcb19a12858
chronaxis <thewrinklycheese@gmail.com>
2022-07-26 23:13:21 -0400

    MainMenu

--- Ascent/Ascent/ViewController.swift
+++ Ascent/Ascent/ViewController.swift
@@ -9,9 +9,14 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBAction func SetTimer(_ sender: UIButton) {
+        
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
     }
 
 
